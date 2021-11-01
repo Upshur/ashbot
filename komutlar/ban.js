6f47d1cea0f6ff00113ef93740f36ce01f45614a
Glitch (ashh) <none>
2021-11-01 21:48:47 +0000

    💎⏳ Checkpoint ./events/ready.js:38223734/176 ./komutlar/ban.js:38223734/869 ./package.json:38223734/2253

--- komutlar/ban.js
+++ komutlar/ban.js
@@ -1,61 +1,28 @@
-const Discord = require("discord.js");
-const db = require("quick.db");
-module.exports.run = async (bot, message, args) => {
-  let prefix = (await db.fetch(`prefix_${message.guild.id}`)) || "d!";
-  if (!message.member.hasPermission("BAN_MEMBERS")) {
-    const embed = new Discord.MessageEmbed()
-      .setDescription("**Ne yazık ki bu komutu kullanmaya yetkin yok.**")
-      .setColor("BLACK");
-
-    message.channel.send(codwaembed);
-    return;
+const { MessageButton } = require('discord-buttons');
+const discord = require('discord.js');
+exports.run = async(client, message, args) => {
+let member = message.guild.member(message.mentions.users.first() || message.guild.members.cache.get(args[0]));
+  let reyn = message.guild.member(member)
+    const embed = new discord.MessageEmbed()
+    .setDescription('Banlamak İçin Aşşağıdaki Butona Tıkla')
+  .setTitle('BANNN :yum:');
+let enginar = new MessageButton()
+  .setStyle('green')
+  .setLabel('BANLA')
+  .setID("click_to_function")
+message.channel.send({ button: enginar, embed: embed });
+client.on('clickButton', async (button) => {
+  if (button.id == "click_to_function") {
+    message.guild.member(reyn.id).ban();
   }
-
-  let u = message.mentions.users.first();
-  if (!u) {
-    return message.channel.send(
-      new Discord.MessageEmbed()
-        .setDescription("Lütfen Sunucudan Banlanıcak Kişiyi Ktiketleyiniz!")
-        .setColor("BLACK")
-        .setFooter(bot.user.username, bot.user.avatarURL)
-    );
-  }
-
-  const codwaembed = new Discord.MessageEmbed()
-    .setColor("BLACK")
-    .setDescription(` ${u} **Adlı şahsın sunucudan banlanmasını onaylıyor musunuz?**`)
-    .setFooter(bot.user.username, bot.user.avatarURL);
-  message.channel.send(codwaembed).then(async function(sentEmbed) {
-    const emojiArray = ["✅"];
-    const filter = (reaction, user) =>
-      emojiArray.includes(reaction.emoji.name) && user.id === message.author.id;
-    await sentEmbed.react(emojiArray[0]).catch(function() {});
-    var reactions = sentEmbed.createReactionCollector(filter, {
-      time: 30000
-    });
-    reactions.on("end", () => sentEmbed.edit(` **İşlem iptal oldu!**`));
-    reactions.on("collect", async function(reaction) {
-      if (reaction.emoji.name === "✅") {
-        message.channel.send(
-          ` **İşlem onaylandı!** ${u} **adlı şahıs sunucudan banlandı!**`
-        );
-
-        message.guild.member(u).ban();
-      }
-    });
-  });
-};
-
-module.exports.conf = {
-  aliases: [],
-  permLevel: 2,
-  enabled: true,
-  guildOnly: true,
-  kategori: "moderasyon"
-};
-
-module.exports.help = {
-  name: "ban",
-  description: "codwa & ottomancode",
-  usage: "ban"
-};
+});
+}
+exports.conf = {
+    enabled: true,
+    guildOnly: false,
+    permLevel:0,
+    aliases: []
+    }
+    exports.help = {
+    name: "ban"
+    } 
